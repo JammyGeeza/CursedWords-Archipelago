a66c1d2bded92367831569368037dfa6946a4d64
JammyGeeza <jammiedodgers6@hotmail.com>
2026-05-29 11:12:00 +0100

    Prevents character sticker/stamps appearing in shop when their compatible bundles not yet received.

--- Mod/Patches/Character.cs
+++ Mod/Patches/Character.cs
@@ -18,35 +18,79 @@ namespace Mod.Patches
         {
             Logger.LogInfo($"{nameof(Character)}.{nameof(Character.GetCharacterItemTypes)} postfix!");
 
-            //for (int i = __result.Count - 1; i >= 0; i--)
-            //{
-            //    Type type = __result[i];
+            for (int i = __result.Count; i > 0; i--)
+            {
+                Type type = __result[i - 1];
 
-            //    if (!ArchipelagoHelper.HasReceivedItem("Sticker Bundle - Blue Tiles") && Lookups.BlueStickers.Contains(type))
-            //    {
-            //        __result.Remove(type);
-            //    }
+                // If part of blue build and received, skip to next
+                if ((Lookups.BlueBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Blue Stamps")) || (Lookups.BlueBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Blue Stickers")))
+                {
+                    continue;
+                }
 
-            //    if (!ArchipelagoHelper.HasReceivedItem("Sticker Bundle - Number Tiles") && Lookups.NumberStickers.Contains(type))
-            //    {
-            //        __result.Remove(type);
-            //    }
+                // If part of card build and received, skip to next
+                if ((Lookups.CardsBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Card Stamps")) || (Lookups.CardsBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Card Stickers")))
+                {
+                    continue;
+                }
 
-            //    if (!ArchipelagoHelper.HasReceivedItem("Sticker Bundle - Red Tiles") && Lookups.RedStickers.Contains(type))
-            //    {
-            //        __result.Remove(type);
-            //    }
+                // If part of chess build and received, skip to next
+                if ((Lookups.ChessBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Chess Stamps")) || (Lookups.ChessBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Chess Stickers")))
+                {
+                    continue;
+                }
 
-            //    if (!ArchipelagoHelper.HasReceivedItem("Sticker Bundle - Shiny Tiles") && Lookups.ShinyStickers.Contains(type))
-            //    {
-            //        __result.Remove(type);
-            //    }
+                // If part of chess build and received, skip to next
+                if ((Lookups.CurrencyBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Currency Stamps")) || (Lookups.CurrencyBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Currency Stickers")))
+                {
+                    continue;
+                }
 
-            //    if (!ArchipelagoHelper.HasReceivedItem("Sticker Bundle - Void Tiles") && Lookups.VoidStickers.Contains(type))
-            //    {
-            //        __result.Remove(type);
-            //    }
-            //}
+                // If part of cursed build and received, skip to next
+                if ((Lookups.CurseBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Cursed Stamps")) || (Lookups.CurseBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Cursed Stickers")))
+                {
+                    continue;
+                }
+
+                // If part of numbers build and received, skip to next
+                if ((Lookups.NumberBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Number Stamps")) || (Lookups.NumberBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Number Stickers")))
+                {
+                    continue;
+                }
+
+                // If part of reinbow build and received, skip to next
+                if ((Lookups.RainbowBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Rainbow Stamps")) || (Lookups.RainbowBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Rainbow Stickers")))
+                {
+                    continue;
+                }
+
+                // If part of red build and received, skip to next
+                if ((Lookups.RedBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Red Stamps")) || (Lookups.RedBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Red Stickers")))
+                {
+                    continue;
+                }
+
+                // If part of scattered build and received, skip to next
+                if ((Lookups.ScatteredBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Scatter Stamps")) || (Lookups.ScatteredBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Scatter Stickers")))
+                {
+                    continue;
+                }
+
+                // If part of shiny build and received, skip to next
+                if ((Lookups.ShinyBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Shiny Stamps")) || (Lookups.ShinyBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Shiny Stickers")))
+                {
+                    continue;
+                }
+
+                // If part of void build and received, skip to next
+                if ((Lookups.VoidBuildStamps.Contains(type) && ArchipelagoHelper.HasReceivedItem("Void Stamps")) || (Lookups.VoidBuildStickers.Contains(type) && ArchipelagoHelper.HasReceivedItem("Void Stickers")))
+                {
+                    continue;
+                }
+
+                // Otherwise, remove it
+                __result.Remove(type);
+            }
         }
 
         /// <summary>
