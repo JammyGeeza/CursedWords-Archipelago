9a68b5fefca46afd418fd2d09fd6922021bfe296
JammyGeeza <jammiedodgers6@hotmail.com>
2026-08-26 14:32:50 +0100

    FIX: Removed accidental recursion and fixed 'item' type tiles from crashing when Graduation Cap is generated as a fallback.

---
+++ Mod/Patches/ScatteredItemPools.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using Mod.Helpers;
+using Mod.Mappings;
+using Modd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+namespace Mod.Patches
+{
+    [HarmonyPatch(typeof(ScatteredItemPools))]
+    internal class ScatteredItemPools_Patches : PatchBase
+    {
+        /// <summary>
+        /// Override fallback to Graduation Cap for Scattered Items, as it isn't in the emoji dictionary and crashes.
+        /// (I spoke to Skyeward about this, but this scenario should never happen in Vanilla!)
+        /// </summary>
+        [HarmonyPatch("GetRarityWeightedItem")]
+        [HarmonyPostfix]
+        public static bool GetRarityWeightedItem_Postfix(ref Item __result)
+        {
+            Logger.LogInfo($"{nameof(ScatteredItemPools)}.GetRarityWeightedItem postfix!");
+
+            // If graduation cap, override it
+            if (__result is GraduationCap)
+            {
+                // Try to replace with generic mult item
+                __result = ScatteredItemPools.GetRandomGenericMultItem();
+
+                // If it's STILL a graduation cap, try any random item
+                if (__result is GraduationCap)
+                {
+                    __result = ScatteredItemPools.GetRandomItem();
+                }
+            }
+
+            return false;
+        }
+    }
+}
